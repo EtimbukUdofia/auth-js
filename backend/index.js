4cf06a201e3f36ea25d508b746d01c0045a4afe9
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-10-26 22:55:56 +0100

    update index file add port

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -14,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port: ${PORT}`);
 });
