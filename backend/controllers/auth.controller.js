a53bdce83972e3b887b0eab74ca6283a43dce037
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-10-27 00:01:39 +0100

    add signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,58 @@
+import bcrypt from "bcryptjs";
+import { User } from "../models/User.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail } from "../mailtrap/emails.js";
+
 export const signup = async (req, res) => {
-  res.send("signup route");
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      // return res.status(400).json({ success: false, message: "User already exists" });
+      throw new Error("User already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    });
+
+    const result = await user.save();
+
+    // jwt
+    generateTokenAndSetCookie(res, user._id);
+
+    // verify emails
+    await sendVerificationEmail(user.email, verificationToken);
+
+    res.status(201).json({
+      success: true,
+      message: "User created succesfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+
+    console.log(result);
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
