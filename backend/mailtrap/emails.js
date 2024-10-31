1928e5e49c2e62e9e9dd722ce3a18b4264d8e181
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-10-31 12:16:33 +0100

    add function to send welcome email

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -9,13 +9,37 @@ export const sendVerificationEmail = async (email, verificationToken) => {
       from: sender,
       to: recipient,
       subject: "Verify your email",
-      html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
-      category: "Email Verification"
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
     });
 
     console.log("Email sent successfully", response);
   } catch (error) {
     console.error(`Error sending verification`, error);
-    throw new Error(`Error sending verification email: ${error}`)
+    throw new Error(`Error sending verification email: ${error}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, username) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailTrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "bb28a056-1922-4eba-b0ab-59b8501fe3d3",
+      template_variables: {
+        company_info_name: sender.name,
+        name: username,
+      },
+    });
+
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email: ${error}`);
+    throw new Error(`Error sending welcome email: ${error}`);
   }
 };
