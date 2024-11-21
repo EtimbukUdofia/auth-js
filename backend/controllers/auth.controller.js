d689a666b3544256f158b8fd07aa1ef56cf53777
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-11-21 10:06:27 +0100

    add reset password functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,7 +3,12 @@ import crypto from "crypto";
 
 import { User } from "../models/User.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail, sendWelcomeEmail, sendPasswordResetEmail } from "../mailtrap/emails.js";
+import {
+  sendVerificationEmail,
+  sendWelcomeEmail,
+  sendPasswordResetEmail,
+  sendResetSuccessEmail,
+} from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -177,6 +182,38 @@ export const forgotPassword = async (req, res) => {
     res.status(200).json({ success: true, message: "Password reset link sent successfully" });
   } catch (error) {
     console.log("Error in forgotPassword ", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ success: false, message: "Password and Confirm Password fields do not match" });
+    }
+
+    const user = await User.findOne({ resetPasswordToken: token, resetPasswordExpiresAt: { $gt: Date.now() } });
+
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Invalid or expired reset token" });
+    }
+
+    //update password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiresAt = undefined;
+    await user.save();
+
+    await sendResetSuccessEmail(user.email);
+
+    res.status(200).json({ success: true, message: "Password reset successful" });
+  } catch (error) {
+    console.error("Error in resetPassword", error);
     res.status(400).json({ success: false, message: error.message });
   }
 };
