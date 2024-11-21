22e5947163a26369b634cdca0a6157ea88dfe6b8
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-11-21 10:04:52 +0100

    update routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,6 +5,7 @@ import {
   signup,
   verifyEmail,
   forgotPassword,
+  resetPassword,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
@@ -16,6 +17,8 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
+
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
